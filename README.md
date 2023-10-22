fa775056a4db4c76f426652730368bbc463731c6
Aayush Agnihotri <aa2328@cornell.edu>
2023-10-22 17:51:08 -0400

    Adding terminal prompts
    - Added prompts welcome the user to blackjack to make it easier to test the project later on

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Team Members:
 Aayush Agnihotri (aa2328)
 Ram Peddu (rbp94)
+
+To see lines of code, install cloc and run:
+cloc --by-file --include-lang=OCaml .
--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+(* let rec repl (eval : string -> string) : unit = print_string "> "; let input
+   = read_line () in match input with | "" -> print_endline "bye" | _ -> input
+   |> eval |> print_endline; repl eval *)
+
+let () =
+  print_endline "Welcome to Blackjack!";
+  print_endline "How many players do you want to play against?";
+  print_string "> ";
+  let input = read_line () in
+  print_endline "Initializing game...";
+  print_endline input
