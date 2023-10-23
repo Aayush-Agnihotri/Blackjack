812f59855cdd5a44b082db8c0f8204ce9492b540
Aayush Agnihotri <aa2328@cornell.edu>
2023-10-23 12:33:48 -0400

    Editing print_card_template for SpotCard and creating print_card_template for FaceCard
    - Changing spacing and formatting of print_card_template for SpotCard
    - Created print and print helping function for FaceCard

--- lib/card.ml
+++ lib/card.ml
@@ -33,62 +33,71 @@ module SpotCard : Card = struct
   let print_card_template n symbol =
     match n with
     | "2" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
         print_endline ("|  " ^ symbol ^ "  |");
         print_endline "|     |";
         print_endline ("|  " ^ symbol ^ "  |");
         print_endline "|____Z|"
     | "3" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline "|     |";
         print_endline ("|  " ^ symbol ^ "  |");
         print_endline "|____e|"
     | "4" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline "|     |";
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline "|____h|"
     | "5" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline ("|  " ^ symbol ^ "  |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline "|____S|"
     | "6" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline "|____9|"
     | "7" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
-        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
         print_endline "|____L|"
     | "8" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
-        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
         print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
-        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
         print_endline "|____8|"
     | "9" ->
-        print_endline " _____";
+        print_endline " _____ ";
         print_endline ("|" ^ n ^ "    |");
-        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
-        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
-        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
         print_endline "|____6|"
-    | _ -> print_endline ""
+    | "10" ->
+        print_endline " _____ ";
+        print_endline ("|" ^ n ^ " " ^ symbol ^ " |");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
+        print_endline ("|" ^ symbol ^ " " ^ symbol ^ " " ^ symbol ^ "|");
+        print_endline "|___0I|"
+    | _ ->
+        print_endline
+          ("Unable to represent card of number " ^ n ^ " and suit " ^ symbol)
 
   let print card =
     match card with
@@ -96,18 +105,32 @@ module SpotCard : Card = struct
         print_card_template (string_of_int number)
           (match suit with
           | Clubs -> "&"
-          | Diamonds -> "^"
-          | Hearts -> "o"
-          | Spades -> "v")
+          | Diamonds -> "o"
+          | Hearts -> "v"
+          | Spades -> "^")
 end
 
 module FaceCard : Card = struct
   type t = {
     face : face;
     suit : suit;
-    number : int;
   }
 
-  let create d = { face = d.face; suit = d.suit; number = 1 }
-  let print d = print_endline (string_of_int d.number)
+  let create d = { face = d.face; suit = d.suit }
+
+  let print_card_template (f : face) (s : suit) =
+    match f with
+    | Jack -> print_endline ""
+    | Queen -> print_endline ""
+    | King -> print_endline ""
+    | Ace -> (
+        match s with
+        | Clubs -> print_endline ""
+        | Diamonds -> print_endline ""
+        | Hearts -> print_endline ""
+        | Spades -> print_endline "")
+
+  let print card =
+    match card with
+    | { face; suit } -> print_card_template face suit
 end
