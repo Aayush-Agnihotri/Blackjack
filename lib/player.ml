6fa2689bc92557b3ecee803a331bf0bb4cc42461
Ram <rbpeddu@gmail.com>
2023-10-22 21:16:00 -0400

    Deleted two uneccesary .mli files and finished base implementation of the player profile.
    Refactored code to get ride of .mli that was not needed. Implemented a player in the game.

---
+++ lib/player.ml
@@ -0,0 +1,13 @@
+module type Playerprofile = sig
+  (** Itteger val that represents the players current  value of their cards*)
+  val sum : int ref
+  (** Function that allows a user to add a card value to the current sum*)
+  val add_to_sum: int -> unit 
+  (**Checks wheter the player should draw another card *)
+  val  check_more: bool
+end
+module Player : Playerprofile = struct
+  let sum = ref 0
+  let add_to_sum (num:int)= sum:= !sum +num 
+  let check_more= false; 
+end
