2f1718561822038fa80096641d98d15aaa9447be
Aayush Agnihotri <aa2328@cornell.edu>
2023-10-23 12:17:51 -0400

    Adding terminal printing for spot cards

--- lib/card.ml
+++ lib/card.ml
@@ -30,24 +30,75 @@ module SpotCard : Card = struct
 
   let create d = { number = d.number; suit = d.suit }
 
-  let print d =
-    print_endline "------------------------";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_string "          ";
-    print_string (string_of_int d.number);
-    print_string "          ";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "|                      |";
-    print_endline "------------------------"
+  let print_card_template n symbol =
+    match n with
+    | "2" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("|  " ^ symbol ^ "  |");
+        print_endline "|     |";
+        print_endline ("|  " ^ symbol ^ "  |");
+        print_endline "|____Z|"
+    | "3" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline "|     |";
+        print_endline ("|  " ^ symbol ^ "  |");
+        print_endline "|____e|"
+    | "4" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline "|     |";
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline "|____h|"
+    | "5" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline ("|  " ^ symbol ^ "  |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline "|____S|"
+    | "6" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline "|____9|"
+    | "7" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline "|____L|"
+    | "8" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ " " ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline "|____8|"
+    | "9" ->
+        print_endline " _____";
+        print_endline ("|" ^ n ^ "    |");
+        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline ("| " ^ symbol ^ symbol ^ symbol ^ " |");
+        print_endline "|____6|"
+    | _ -> print_endline ""
+
+  let print card =
+    match card with
+    | { number; suit } ->
+        print_card_template (string_of_int number)
+          (match suit with
+          | Clubs -> "&"
+          | Diamonds -> "^"
+          | Hearts -> "o"
+          | Spades -> "v")
 end
 
 module FaceCard : Card = struct
