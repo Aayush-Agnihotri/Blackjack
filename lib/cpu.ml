5047d40a7b5fb3946d391d5301603330609c78b3
Nathan Palamuttam <npp8@cornell.edu>
2023-10-23 00:32:07 -0400

    change module name for CPU

--- lib/cpu.ml
+++ lib/cpu.ml
@@ -1,4 +1,4 @@
-module type Playerprofile = sig
+module type CPUprofile = sig
   (** Itteger val that represents the players current  value of their cards*)
   val sum : int ref
   (** Function that allows a user to add a card value to the current sum*)
@@ -6,7 +6,7 @@ module type Playerprofile = sig
   (**Checks wheter the player should draw another card *)
   val  check_more: bool
 end
-module Player : Playerprofile = struct
+module CPU : CPUprofile = struct
   let sum = ref 0
   let add_to_sum (num:int)= sum:= !sum +num 
   let check_more= if !sum < 17 then true else false; 
