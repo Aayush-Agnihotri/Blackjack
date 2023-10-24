6e7c25ec9b88674fc2eae438d933cdb3954cb8f6
Aayush Agnihotri <aa2328@cornell.edu>
2023-10-23 21:05:30 -0400

    Adding basic repl for demo
    - Added prompts to allow the user to hit, stay, or exit

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,30 @@
 open Blackjack
-(* let rec repl (eval : string -> string) : unit = print_string "> "; let input
-   = read_line () in match input with | "" -> print_endline "bye" | _ -> input
-   |> eval |> print_endline; repl eval *)
+
+let rec repl (eval : string -> string) : unit =
+  print_string "> ";
+  let input = read_line () in
+  match input with
+  | "" -> print_endline "bye"
+  | _ ->
+      input |> eval |> print_endline;
+      repl eval
 
 module Test_Card = Card.SpotCard
 
 type a = Test_Card.suit
 
+let all_cards = []
+let player_cards = []
+let _ = Random.self_init ()
+
 let () =
-  print_endline "Welcome to Blackjack!";
-  print_endline "How many players do you want to play against?";
-  print_string "> ";
+  print_endline "\n\nWelcome to Blackjack!\n";
+  print_endline "Press H to hit, S to stay, or E to exit";
   let input = read_line () in
-  Test_Card.print
-    (Test_Card.create (int_of_string input) Test_Card.Clubs Test_Card.None)
+  match input with
+  | "H" ->
+      print_endline "You chose to hit";
+      let rand_num = Random.int 10 in
+      Test_Card.print (Test_Card.create rand_num Test_Card.Clubs Test_Card.None)
+  | "S" -> print_endline "You chose to stay"
+  | _ -> print_endline "Thanks for playing"
